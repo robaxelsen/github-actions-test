005cb6b770564385a60c47fb196f43ccd73cfb12
Robert Axelsen <robert.axelsen@ebcont.com>
2020-06-23 15:05:31 +0200

    log out from open issue JS action to debug

--- .github/actions/issue/index.js
+++ .github/actions/issue/index.js
@@ -1,5 +1,5 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
+// const github = require('@actions/github');
 
 try {
     const token = core.getInput('token');
@@ -7,16 +7,17 @@ try {
     const body = core.getInput('body');
     const assignees = core.getInput('assignees');
 
-    const octokit = new github.getOctokit(token);
+    // const octokit = new github.getOctokit(token);
+    //
+    // const response = octokit.issues.create({
+    //     ...github.context.repo,
+    //     title,
+    //     body,
+    //     assignees: assignees ? assignees.split('\n') : undefined
+    // })
 
-    const response = octokit.issues.create({
-        ...github.context.repo,
-        title,
-        body,
-        assignees: assignees ? assignees.split('\n') : undefined
-    })
-
-    core.setOutput('issue', JSON.stringify(response.data))
+    // core.setOutput('issue', JSON.stringify(response.data))
+    core.setOutput('issue', `token: ${token}, title: ${title}, body: ${body}, assignees: ${assignees}`);
 } catch(error) {
     core.setFailed(error.message)
 }
